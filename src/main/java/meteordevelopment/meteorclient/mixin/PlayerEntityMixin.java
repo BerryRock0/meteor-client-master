098e22540a9ff13d0a23838e8fadda60da179788
BerryRock0 <mdenials128@gmail.com>
2026-01-02 14:27:27 +0500

    Update PlayerEntityMixin.java

--- src/main/java/meteordevelopment/meteorclient/mixin/PlayerEntityMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/PlayerEntityMixin.java
@@ -115,11 +115,11 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 
     @ModifyReturnValue(method = "getBlockInteractionRange", at = @At("RETURN"))
     private double modifyBlockInteractionRange(double original) {
-        return Math.max(0, original + Modules.get().get(Reach.class).blockReach());
+        return original + Modules.get().get(Reach.class).blockReach();
     }
 
     @ModifyReturnValue(method = "getEntityInteractionRange", at = @At("RETURN"))
     private double modifyEntityInteractionRange(double original) {
-        return Math.max(0, original + Modules.get().get(Reach.class).entityReach());
+        return original + Modules.get().get(Reach.class).entityReach();
     }
 }
