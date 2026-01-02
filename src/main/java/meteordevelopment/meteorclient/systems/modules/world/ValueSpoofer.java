d43ece47f14a37f58607191d1928c2ab522cd5e3
BerryRock0 <mdenials128@gmail.com>
2026-01-02 17:06:58 +0500

    Update ValueSpoofer.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/ValueSpoofer.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/ValueSpoofer.java
@@ -11,14 +11,48 @@ import meteordevelopment.meteorclient.utils.Utils;
 
 public class ValueSpoofer extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-
+    
     private final Setting<Double> distanceToEntity = sgGeneral.add(new DoubleSetting.Builder()
         .name("distance-to-entity")
         .description("Distance to entities value")
         .build()
     );
 
+   public final Setting<Set<EntityType<?>>> entities = sgGeneral.add(new EntityTypeListSetting.Builder()
+        .name("entities")
+        .description("Select specific entities.")
+        .build()
+    );
+
+    private final Setting<Boolean> cases = sgBounds.add(new BoolSetting.Builder()
+        .name("cases")
+        .description("Disables/enables black/white cases.")
+        .defaultValue(false)
+        .build()
+    );
+
     public ValueSpoofer() {
         super(Categories.World, "valuespoofer", "Spoofs values.");
+    }
+    
+    public boolean inList(Entity entity)
+    {
+        return isActive() && inFrames(entity);
+    }
+
+    public boolean inFrames(Entity entity)
+    {
+        switch(filter.get())
+        {
+            case White -> {return entities.get().contains(entity.getType());}
+            case Black -> {return !entities.get().contains(entity.getType());}
+        }
+        return cases.get();
+    }
+
+    public enum Filter
+    {
+        White,
+        Black
     }
 }
