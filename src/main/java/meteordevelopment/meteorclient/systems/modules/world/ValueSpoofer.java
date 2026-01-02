c464782ae232f665b25137eff8283e9a518e7ff5
BerryRock0 <mdenials128@gmail.com>
2026-01-02 16:37:54 +0500

    Create ValueSpoofer.java

---
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/ValueSpoofer.java
@@ -0,0 +1,23 @@
+package meteordevelopment.meteorclient.systems.modules.player;
+
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.widgets.WWidget;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.Utils;
+
+public class Reach extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Double> distanceToEntity = sgGeneral.add(new DoubleSetting.Builder()
+        .name("distance-to-entity")
+        .description("Distance to entities value")
+        .build()
+    );
+
+    public Reach() {
+        super(Categories.Player, "reach", "Gives you super long arms.");
+    }
