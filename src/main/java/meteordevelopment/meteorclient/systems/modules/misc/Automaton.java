74487f9ab5db75c2c25cb738e8162f25a2a0a2b2
BerryRock0 <mdenials128@gmail.com>
2026-01-24 16:33:48 +0500

    Add files via upload

---
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/Automaton.java
@@ -0,0 +1,48 @@
+package meteordevelopment.meteorclient.systems.modules.misc;
+
+import java.util.List;
+
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Automaton extends Module
+{
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<List<String>> commands = sgGeneral.add(new StringListSetting.Builder()
+        .name("commands")
+        .description("setting commands")
+        .build()
+    );
+
+    public Automaton()
+    {
+        super(Categories.Misc, "automaton", "Doing actions with instructions.");
+    }
+
+    public void main()
+    {
+        for(String cmd : commands.get())
+          if(isActive())
+            parseAndProceed(cmd);
+    }
+
+    public void parseAndProceed(String input)
+    {
+        String[] parts = input.trim().split("\\s+");
+
+        switch (parts[0].toLowerCase())
+        {
+            case "set_yaw": MC.player.setYaw(parts[1].toLowerCase());
+            case "set_pitch": MC.player.setPitch(parts[1].toLowerCase());
+            case "press_forward": mc.options.forwardKey.setPressed(Boolean.parseBoolean(parts[1].toLowerCase()));
+            case "press_back": mc.options.backKey.setPressed(Boolean.parseBoolean(parts[1].toLowerCase()));
+            case "press_left": mc.options.leftKey.setPressed(Boolean.parseBoolean(parts[1].toLowerCase()));
+            case "press_right": mc.options.rightKey.setPressed(Boolean.parseBoolean(parts[1].toLowerCase()));
+            case "press_jump": mc.options.rightKey.setPressed(Boolean.parseBoolean(parts[1].toLowerCase()));
+        }
+    }
+}	
