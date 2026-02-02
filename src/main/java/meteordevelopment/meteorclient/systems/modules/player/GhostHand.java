f64d0618fdbacfe961f608f0e4ea3895de3e1c2a
BerryRock0 <mdenials128@gmail.com>
2026-02-02 17:58:15 +0500

    Update GhostHand.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/GhostHand.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/GhostHand.java
@@ -6,22 +6,15 @@
 package meteordevelopment.meteorclient.systems.modules.player;
 
 import java.util.List;
-import java.util.Set;
 
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import net.minecraft.block.Block;
+import net.minecraft.util.shape.VoxelShapes;
+
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
-import meteordevelopment.meteorclient.settings.BlockListSetting;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import net.minecraft.block.Block;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 
 public class GhostHand extends Module {
 	
