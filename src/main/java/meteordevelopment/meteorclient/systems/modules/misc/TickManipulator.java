f83e1b837465e079abaf41df6fa9e23c8126865d
BerryRock0 <mdenials128@gmail.com>
2026-02-05 16:23:36 +0500

    Update TickManipulator.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/TickManipulator.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/TickManipulator.java
@@ -14,13 +14,22 @@ public class TickManipulator extends Module
     private final SettingGroup sgClient = settings.createGroup("Client");
     private final SettingGroup sgServer = settings.createGroup("Server");
 
-	public final Setting clientdelay = sgClient.add(new IntSetting.Builder().name("client-delay").description("update client timer.").defaultValue(0).build());
-	public final Setting serverdelay = sgServer.add(new IntSetting.Builder().name("server-delay").description("update server timer.").defaultValue(0).build());
+    public final Setting beginclientdelay = sgClient.add(new IntSetting.Builder().name("begin-client-delay").description("client timer begin value.").defaultValue(0).build());
+    public final Setting endclientdelay = sgClient.add(new IntSetting.Builder().name("end-client-delay").description("client timer begin value.").defaultValue(0).build());
+    public final Setting clientbool = sgClient.add(new BoolSetting.Builder().name("client-bool").description("update client boolean.").defaultValue(false).build());
+    public final Setting clientincrement = sgClient.add(new BoolSetting.Builder().name("client-increment").description("increment server time value.").defaultValue(false).build());
+    public final Setting clientdecrement = sgClient.add(new BoolSetting.Builder().name("client-decrement").description("decrement server time value.").defaultValue(false).build());
+    public final Setting clientqueue = sgClient.add(new BoolSetting.Builder().name("client-queue").description("in queue returning boolean value.").defaultValue(false).build());
+    public final Setting clientalways = sgClient.add(new BoolSetting.Builder().name("client-always").description("in iterate returning boolean value.").defaultValue(false).build());
 
-	public final Setting clientbool = sgClient.add(new BoolSetting.Builder().name("client-bool").description("update client boolean.").defaultValue(false).build());
+    public final Setting beginserverdelay = sgServer.add(new IntSetting.Builder().name("begin-server-delay").description("server timer begin value.").defaultValue(0).build());
+    public final Setting endserverdelay = sgServer.add(new IntSetting.Builder().name("end-server-delay").description("server timer begin value.").defaultValue(0).build());
 	public final Setting serverbool = sgServer.add(new BoolSetting.Builder().name("server-bool").description("update server boolean.").defaultValue(false).build());
-
-
+    public final Setting serverincrement = sgServer.add(new BoolSetting.Builder().name("server-increment").description("increment server time value.").defaultValue(false).build());
+    public final Setting serverdecrement = sgServer.add(new BoolSetting.Builder().name("server-decrement").description("decrement server time value.").defaultValue(false).build());
+    public final Setting serverqueue = sgClient.add(new BoolSetting.Builder().name("server-queue").description("queue returning boolean value.").defaultValue(false).build());
+    public final Setting serveralways = sgClient.add(new BoolSetting.Builder().name("server-always").description("always returning boolean value.").defaultValue(false).build());
+    
 	public int clientTimer;
     public int serverTimer;
 
@@ -32,24 +41,27 @@ public class TickManipulator extends Module
 	public boolean clientTime()
     {
         // wait for timer
-		if(clientTimer > 0 && Boolean.TRUE.equals(clientbool.get()))
+		if(clientTimer != endclientdelay.get() && Boolean.TRUE.equals(clientbool.get()))
 		{
-			clientTimer--;
-			return true;
+			if (clientincrement.get()) clientTimer--;
+            if (clientdecrement.get()) clientTimer++;
+			return clientqueue.get();
 		}
-		clientTimer = (Integer)clientdelay.get();
-        return false;
+		clientTimer = (Integer)beginclientdelay.get();
+        return clientalways.get();
     }
 
     public boolean serverTime()
     {
         // wait for timer
-		if(serverTimer > 0 && Boolean.TRUE.equals(serverbool.get()))
+		if(serverTimer != endserverdelay.get() && Boolean.TRUE.equals(serverbool.get()))
 		{
-			serverTimer--;
-			return true;
+			if (serverincrement.get()) serverTimer--;
+            if (serverdecrement.get()) serverTimer++;
+			return serverqueue.get();
 		}
-		serverTimer = (Integer)serverdelay.get();
-        return false;
+		serverTimer = (Integer)beginserverdelay.get();
+        return serveralways.get();
     }
+    
 }
