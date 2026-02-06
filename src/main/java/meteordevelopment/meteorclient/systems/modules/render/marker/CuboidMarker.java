96b61e9b14ef247ab88e22d7a887844a7162f743
BerryRock0 <mdenials128@gmail.com>
2026-02-06 20:24:46 +0500

    Update CuboidMarker.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/marker/CuboidMarker.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/marker/CuboidMarker.java
@@ -5,25 +5,10 @@
 
 package meteordevelopment.meteorclient.systems.modules.render.marker;
 
-import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
-import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.renderer.ShapeMode;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
-import meteordevelopment.meteorclient.utils.world.BlockUtils;
-import meteordevelopment.meteorclient.gui.GuiTheme;
-import meteordevelopment.meteorclient.gui.widgets.WWidget;
-import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
-import net.minecraft.block.Block;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
-
 // TODO: Add outline and more modes
 public class CuboidMarker extends BaseMarker {
     public static final String type = "Cuboid";
@@ -34,8 +19,6 @@ public class CuboidMarker extends BaseMarker {
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     private final SettingGroup sgRender = settings.createGroup("Render");
-    private final SettingGroup sgControl = settings.createGroup("Control");
-    private final SettingGroup sgExecution = settings.createGroup("Execution");
 
     // General
 
@@ -81,32 +64,6 @@ public class CuboidMarker extends BaseMarker {
         .build()
     );
 
-    private final Setting<Boolean> mining = sgExecution.add(new BoolSetting.Builder()
-        .name("Mining")
-        .description("Break blocks in area")
-        .defaultValue(false)
-        .build()
-    );
-    private final Setting<Boolean> using = sgExecution.add(new BoolSetting.Builder()
-        .name("Using")
-        .description("Intreact blocks in area")
-        .defaultValue(false)
-        .build()
-    );
-
-    private final Setting<Boolean> pre = sgControl.add(new BoolSetting.Builder()
-        .name("Pre")
-        .description("Load script before tick.")
-        .defaultValue(false)
-        .build()
-    );
-    private final Setting<Boolean> post = sgControl.add(new BoolSetting.Builder()
-        .name("Post")
-        .description("Load script after tick.")
-        .defaultValue(false)
-        .build()
-    );
-
     public int minX;
     public int minY;
     public int minZ;
@@ -133,44 +90,5 @@ public class CuboidMarker extends BaseMarker {
         maxZ = Math.max(pos1.get().getZ(), pos2.get().getZ());
 
         event.renderer.box(minX, minY, minZ, maxX, maxY, maxZ, sideColor.get(), lineColor.get(), shapeMode.get(), 0);
-    }
-    @EventHandler
-    private void onTickPre(TickEvent.Pre event)
-    {
-        if (pre.get())
-        {
-            if(mining.get()) mine();
-            if(using.get()) interact();      
-        }
-    }
-        
-    @EventHandler
-    private void onTickPre(TickEvent.Post event) 
-    {
-        if (post.get())
-        { 
-            if(mining.get()) mine();
-            if(using.get()) interact(); 
-        }
-    }
-    
-
-    private void mine()
-    {
-        for(int x = minX; x < maxX; x++)
-        for(int y = minY; y < maxY; y++)
-        for(int z = minZ; z < maxZ; z++)
-        BlockUtils.breakBlock(new BlockPos(x,y,z), false);
-    }
-
-    private void interact()
-    {
-        for(int x = minX; x < maxX; x++)
-        for(int y = minY; y < maxY; y++)
-        for(int z = minZ; z < maxZ; z++)
-        {
-            BlockPos pos = new BlockPos(x,y,z);
-            BlockUtils.interact(new BlockHitResult(pos.toCenterPos(), BlockUtils.getDirection(pos), pos, true), Hand.MAIN_HAND, false); 
-        }
     }
 }
