9bb66b3be912db74a0efe81c17dfdf706891ff8e
BerryRock0 <mdenials128@gmail.com>
2026-02-06 20:13:18 +0500

    Add files via upload

---
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/MinerPlacer.java
@@ -0,0 +1,97 @@
+package meteordevelopment.meteorclient.systems.modules.world;
+
+import meteordevelopment.orbit.EventHandler;
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
+import meteordevelopment.meteorclient.utils.world.BlockUtils;
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.widgets.WWidget;
+import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
+import net.minecraft.block.Block;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+
+
+public class MinerPlacer extends Module
+{
+    private final SettingGroup sgMiner = settings.createGroup("Miner");
+    private final SettingGroup sgPlacer = settings.createGroup("Placer");
+
+
+    private final Setting<BlockPos> mine = sgGeneral.add(new BlockPosSetting.Builder()
+        .name("mine-pos")
+        .description("Mining block position")
+        .build()
+    );
+
+    private final Setting<BlockPos> interact = sgGeneral.add(new BlockPosSetting.Builder()
+        .name("interact-pos")
+        .description("Interact block position")
+        .build()
+    );
+
+
+    private final Setting<Boolean> pre = sgControl.add(new BoolSetting.Builder()
+        .name("Pre")
+        .description("Load script before tick.")
+        .defaultValue(false)
+        .build()
+    );
+    private final Setting<Boolean> post = sgControl.add(new BoolSetting.Builder()
+        .name("Post")
+        .description("Load script after tick.")
+        .defaultValue(false)
+        .build()
+    );
+
+    private final Setting<Boolean> mining = sgExecution.add(new BoolSetting.Builder()
+        .name("Mining")
+        .description("Break blocks in area")
+        .defaultValue(false)
+        .build()
+    );
+    private final Setting<Boolean> using = sgExecution.add(new BoolSetting.Builder()
+        .name("Using")
+        .description("Intreact blocks in area")
+        .defaultValue(false)
+        .build()
+    );
+
+    public MinerPlacer()
+    {
+        super(Categories.World, "MinerPlacer", "Break or Place in specific coordinate.");
+    }
+
+    @EventHandler
+    private void onTickPre(TickEvent.Pre event)
+    {
+        if (pre.get())
+        { 
+            BlockPos minepos = new BlockPos(minepos.get().getX(),minepos.get().getY(), minepos.get().getZ());
+            BlockPos interactpos = new BlockPos(minepos.get().getX(),minepos.get().getY(), minepos.get().getZ());
+            if(mining.get()) BlockUtils.breakBlock(minepos, false);
+            if(using.get()) BlockUtils.interact(new BlockHitResult(interactpos.toCenterPos(), BlockUtils.getDirection(interactpos), interactpos, true), Hand.MAIN_HAND, false); 
+        }
+    }
+        
+    @EventHandler
+    private void onTickPre(TickEvent.Post event) 
+    {
+        if (post.get())
+        {
+            BlockPos minepos = new BlockPos(minepos.get().getX(),minepos.get().getY(), minepos.get().getZ());
+            BlockPos interactpos = new BlockPos(minepos.get().getX(),minepos.get().getY(), minepos.get().getZ());
+            if(mining.get()) BlockUtils.breakBlock(minepos, false);
+            if(using.get()) BlockUtils.interact(new BlockHitResult(interactpos.toCenterPos(), BlockUtils.getDirection(interactpos), interactpos, true), Hand.MAIN_HAND, false); 
+        }
+    }
+
+}
