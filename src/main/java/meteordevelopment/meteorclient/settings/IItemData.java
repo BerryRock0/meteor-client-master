9d7f22c34a34c7471421985dd8be6ca145bc2588
BerryRock0 <mdenials128@gmail.com>
2026-02-11 09:42:56 -0500

    Add files via upload

---
+++ src/main/java/meteordevelopment/meteorclient/settings/IItemData.java
@@ -0,0 +1,13 @@
+package meteordevelopment.meteorclient.settings;
+
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.WidgetScreen;
+import meteordevelopment.meteorclient.utils.misc.IChangeable;
+import meteordevelopment.meteorclient.utils.misc.ICopyable;
+import meteordevelopment.meteorclient.utils.misc.ISerializable;
+import net.minecraft.item.Item;
+
+public interface IItemData<T extends ICopyable<T> & I Serializable<T> & IChangeable & IItemData<T>>
+{
+    WidgetScreen createScreen (GuiTheme theme, Item item, ItemDataSetting<T> setting)
+}
