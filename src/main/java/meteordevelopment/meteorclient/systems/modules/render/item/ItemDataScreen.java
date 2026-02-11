fd86fa41af3b657e6c759114904db17331a2b170
BerryRock0 <mdenials128@gmail.com>
2026-02-11 10:32:41 -0500

    Add files via upload

---
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/item/ItemDataScreen.java
@@ -0,0 +1,75 @@
+package meteordevelopment.meteorclient.systems.modules.render.blockesp;
+
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.WindowScreen;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
+import net.minecraft.item.Item;
+import org.jetbrains.annotations.Nullable;
+
+public class ItemDataScreen extends WindowScreen
+{
+    private final ItemData itemData;
+    private final Setting<?> setting;
+    private final @Nullable Runnable firstChangeConsumer;
+
+    public ItemDataScreen(GuiTheme theme, ItemData itemData, Item item, ItemDataSetting<ItemData> setting)
+    {
+        this(theme, itemData, setting, () -> setting.get().put(item, blockData));
+    }
+
+
+    public ItemDataScreen(GuiTheme theme, ItemData blockData, GenericSetting<ItemData> setting)
+    {
+        this(theme, blockData, setting, null);
+    }
+
+
+    private ESPBlockDataScreen(GuiTheme theme, ItemData itemData, Setting<?> setting, @Nullable Runnable firstChangeConsumer)
+    {
+        super(theme, "Configure Item");
+
+        this.itemData = itemData;
+        this.setting = setting;
+        this.firstChangeConsumer = firstChangeConsumer;
+    }
+
+    @Override
+    public void initWidgets()
+    {
+        Settings settings = new Settings();
+        SettingGroup sgGeneral = settings.getDefaultGroup();
+
+        sgGeneral.add(new ColorSetting.Builder()
+            .name("item-color")
+            .description("Color of items.")
+            .defaultValue(new SettingColor(0, 0, 0, 0))
+            .onModuleActivated(settingColorSetting -> settingColorSetting.get().set(itemData.itemColor))
+            .onChanged(settingColor ->
+            {
+                if (!itemData.itemColor.equals(settingColor))
+                {
+                    itemData.itemColor.set(settingColor);
+                    onChanged();
+                }
+            })
+            .build()
+        );
+
+        settings.onActivated();
+        add(theme.settings(settings)).expandX();
+    }
+
+        private void onChanged()
+        {
+            if (!blockData.isChanged() && firstChangeConsumer != null)
+            {
+                firstChangeConsumer.run();
+            }
+
+            setting.onChanged();
+            blockData.changed();
+        }
+}
+
