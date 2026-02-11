2f0a3702abcaa7aeeac0ce8f3f2f2e25638f3e15
BerryRock0 <mdenials128@gmail.com>
2026-02-11 23:35:48 +0500

    Create ItemDataSettingScreen.java

---
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/settings/ItemDataSettingScreen.java
@@ -0,0 +1,72 @@
+package meteordevelopment.meteorclient.gui.screens.settings;
+
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.renderer.GuiRenderer;
+import meteordevelopment.meteorclient.gui.screens.settings.base.CollectionMapSettingScreen;
+import meteordevelopment.meteorclient.gui.widgets.WWidget;
+import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
+import meteordevelopment.meteorclient.settings.ItemDataSetting;
+import meteordevelopment.meteorclient.settings.IItemData;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.IVisible;
+import meteordevelopment.meteorclient.utils.misc.IGetter;
+import meteordevelopment.meteorclient.utils.misc.IChangeable;
+import meteordevelopment.meteorclient.utils.misc.ICopyable;
+import meteordevelopment.meteorclient.utils.misc.ISerializable;
+import meteordevelopment.meteorclient.utils.misc.Names;
+import net.minecraft.item.Item;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.registry.Registries;
+import org.jetbrains.annotations.Nullable;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
+public class ItemDataSettingScreen<T extends ICopyable<T> & ISerializable<T> & IChangeable & IItemData<T>> extends CollectionMapSettingScreen<Item, T> {
+    private final ItemDataSetting<T> setting;
+    private boolean invalidate;
+
+    public ItemDataSettingScreen(GuiTheme theme, ItemDataSetting<T> setting) {
+        super(theme, "Configure Items", setting, setting.get(), Registries.ITEM);
+
+        this.setting = setting;
+    }
+
+    @Override
+    protected boolean includeValue(Block value) {
+        return value != null;
+    }
+
+    @Override
+    protected WWidget getValueWidget(Item item) {
+        return theme.itemWithLabel(block.asItem().getDefaultStack(), Names.get(item));
+    }
+
+    @Override
+    protected WWidget getDataWidget(Item item, @Nullable T itemData) {
+        WButton edit = theme.button(GuiRenderer.EDIT);
+        edit.action = () -> {
+            T data = itemData;
+            if (data == null) data = setting.defaultData.get().copy();
+
+            mc.setScreen(data.createScreen(theme, item, setting));
+            invalidate = true;
+        };
+        return edit;
+    }
+
+    @Override
+    protected void onRenderBefore(DrawContext drawContext, float delta) {
+        if (invalidate) {
+            this.invalidateTable();
+            invalidate = false;
+        }
+    }
+
+    @Override
+    protected String[] getValueNames(Block block) {
+        return new String[]{
+            Names.get(item),
+            Registries.ITEM.getId(item).toString()
+        };
+    }
+}
