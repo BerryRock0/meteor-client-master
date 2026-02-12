a4b7ccee86a1218eb901899ce68a18967945d914
BerryRock0 <mdenials128@gmail.com>
2026-02-12 16:16:04 +0500

    Update Modules.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
@@ -544,7 +544,6 @@ public class Modules extends System<Modules> {
         add(new EChestFarmer());
         add(new EndermanLook());
         add(new Flamethrower());
-        add(new HighwayBuilder());
         add(new LiquidFiller());
         add(new MinerPlacer());
         add(new MountBypass());
