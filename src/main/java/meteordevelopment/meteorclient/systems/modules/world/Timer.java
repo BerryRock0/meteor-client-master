cfc563e386a7008070cb3606d68e4de1a8957a8e
BerryRock0 <mdenials128@gmail.com>
2026-02-12 17:12:40 +0500

    Update Timer.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/Timer.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/Timer.java
@@ -6,6 +6,7 @@
 package meteordevelopment.meteorclient.systems.modules.world;
 
 import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.EnumSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
@@ -14,12 +15,17 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 public class Timer extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Double> multiplier = sgGeneral.add(new DoubleSetting.Builder()
-        .name("multiplier")
-        .description("The timer multiplier amount.")
+    private final Setting<Double> doubleValue = sgGeneral.add(new DoubleSetting.Builder()
+        .name("value")
+        .description("The timer value amount.")
         .defaultValue(1)
-        .min(0.1)
-        .sliderMin(0.1)
+        .build()
+    );
+    
+    private final Setting<Rotating> mode = sgGeneral.add(new EnumSetting.Builder<Rotating>()
+        .name("Mode")
+        .description("Timer ticks mode.")
+        .defaultValue(Tick.Multiplication)
         .build()
     );
 
@@ -36,5 +42,25 @@ public class Timer extends Module {
 
     public void setOverride(double override) {
         this.override = override;
+    }
+
+    public int setTick(int a)
+    {
+        switch (mode.get())
+            case Addition -> {a+= doubleValue.get()}
+            case Subtraction -> {a-= doubleValue.get()}
+            case Multiplication -> {a*= doubleValue.get()}
+            case Division -> {a/= doubleValue.get()}
+            case Exponentiation -> a=Math.pow(a, doubleValue.get());
+        return (int) a;      
+    }
+
+    public enum Tick
+    {
+        Addition, 
+        Subtraction,
+        Multiplication,
+        Division,
+        Exponentiation
     }
 }
