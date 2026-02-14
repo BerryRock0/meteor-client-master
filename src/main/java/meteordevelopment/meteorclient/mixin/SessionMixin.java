aa8d62c0cf688537c0eda9080033f3e410f139e9
BerryRock0 <mdenials128@gmail.com>
2026-02-14 18:16:01 +0500

    Update SessionMixin.java

--- src/main/java/meteordevelopment/meteorclient/mixin/SessionMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/SessionMixin.java
@@ -22,56 +22,56 @@ import meteordevelopment.meteorclient.systems.modules.misc.ServerSpoof;
 public abstract class SessionMixin
 {
   @Inject(method = "getAccessToken()Ljava/lang/String;", at=@At("HEAD"))
-  public String spoofAccessToken(String string, CallbackInfoReturnable cir) 
+  public String spoofAccessToken(String string, CallbackInfoReturnable<String> cir) 
   {
     ServerSpoof serverSpoof = Modules.get().get(ServerSpoof.class);
     if(serverSpoof.isActive() && serverSpoof.spoofAccessToken.get())
-      cir.setReturnValue(serverSpoof);
+      cir.setReturnValue(serverSpoof.accessTokenString.get());
     return string;
   }
 
   @Inject(method = "getClientId()Ljava/util/Optional;", at=@At("HEAD"))
-  public String spoofClientId(String string, CallbackInfoReturnable cir)
+  public String spoofClientId(String string, CallbackInfoReturnable<String> cir)
   {
     ServerSpoof serverSpoof = Modules.get().get(ServerSpoof.class);
     if(serverSpoof.isActive() && serverSpoof.spoofClientId.get())
-      cir.setReturnValue(server);
+      cir.setReturnValue(serverSpoof.clientIdString.get());
     return string;
   }
   
   @Inject(method = "getSessionId()Ljava/lang/String;", at=@At("HEAD"))
-  public String spoofSessionId(String string, CallbackInfoReturnable cir)
+  public String spoofSessionId(String string, CallbackInfoReturnable<String> cir)
   {
     ServerSpoof serverSpoof = Modules.get().get(ServerSpoof.class);
     if(serverSpoof.isActive() && serverSpoof.spoofSessionId.get())
-      cir.setReturnValue();
+      cir.setReturnValue(serverSpoof.sessionIdString.get());
     return string;
   }
   
   @Inject(method = "getUsername()Ljava/lang/String;", at=@At("HEAD"))
-  public String spoofUsername(String string, CallbackInfoReturnable cir)
+  public String spoofUsername(String string, CallbackInfoReturnable<String> cir)
   {
     ServerSpoof serverSpoof = Modules.get().get(ServerSpoof.class);
     if(serverSpoof.isActive() && serverSpoof.spoofUsername.get())
-      cir.setReturnValue();
+      cir.setReturnValue(serverSpoof.usernameString.get());
     return string;
   }
   
   @Inject(method = "getUuidOrNull()Ljava/util/UUID;", at=@At("HEAD"))
-  public UUID spoofUuidOrNull(UUID uuid, CallbackInfoReturnable cir)
+  public UUID spoofUuidOrNull(UUID uuid, CallbackInfoReturnable<UUID> cir)
   {
     ServerSpoof serverSpoof = Modules.get().get(ServerSpoof.class);
     if(serverSpoof.isActive() && serverSpoof.spoofUuid.get())
-      cir.setReturnValue();
+      cir.setReturnValue((UUID)serverSpoof.uuidString.get());
     return uuid;
   }
   
   @Inject(method = "getXuid()Ljava/util/Optional;", at=@At("HEAD"))
-  public String spoofXuid(String string, CallbackInfoReturnable cir)
+  public String spoofXuid(String string, CallbackInfoReturnable<String> cir)
   {
     ServerSpoof serverSpoof = Modules.get().get(ServerSpoof.class);
     if(serverSpoof.isActive() && serverSpoof.spoofXuid.get())
-      cir.setReturnValue();
+      cir.setReturnValue(serverSpoof.xuidString.get());
     return string;
   }  
 }
