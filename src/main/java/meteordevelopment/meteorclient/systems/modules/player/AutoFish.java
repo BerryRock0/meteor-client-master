a6517c6faa3aecb143d04199f5ea8fdab5501b13
BerryRock0 <mdenials128@gmail.com>
2026-02-15 18:42:29 +0500

    Update AutoFish.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/AutoFish.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/AutoFish.java
@@ -5,22 +5,22 @@
 
 package meteordevelopment.meteorclient.systems.modules.player;
 
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 
+import meteordevelopment.meteorclient.mixin.FishingBobberEntityAccessor;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.world.TickRate;
 import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.orbit.EventHandler;
 
 
-public class AutoFish extends Module {
+public class AutoFish extends Module
+{
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-
 
     private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
         .name("range")
@@ -28,30 +28,38 @@ public class AutoFish extends Module {
         .defaultValue(0)
         .build()
     );
-    
-    private final Setting<Integer> castValue = sgGeneral.add(new IntSetting.Builder()
+
+    private final Setting<Integer> castDelay = sgGeneral.add(new IntSetting.Builder()
         .name("cast-delay")
         .description("How long to wait between recasts if the bobber fails to land in water.")
         .defaultValue(0)
         .build()
     );
 
-    private final Setting<Integer> catchValue = sgGeneral.add(new IntSetting.Builder()
-        .name("catch-delay")
-        .description("How long to wait after hooking a fish to reel it in.")
+    private final Setting<Integer> castDelayVariance = sgGeneral.add(new IntSetting.Builder()
+        .name("cast-delay-variance")
+        .description("Maximum amount of randomness added to cast delay.")
+        .defaultValue(0)
+        .build()
+    );
+
+    private final Setting<Integer> catchDelayVariance = sgGeneral.add(new IntSetting.Builder()
+        .name("catch-delay-variance")
+        .description("Maximum amount of randomness added to catch delay.")
         .defaultValue(0)
         .build()
     );
 
     public double x,y,z;
-    public int catchDelay;
-    public int castDelay;
-
+    public boolean hooked;
+    private double castDelayLeft = 0.0;
+    private double catchDelayLeft = 0.0;
+    
     public AutoFish()
     {
         super(Categories.Player, "auto-fish", "Automatically fishes for you.");
     }
-    
+
     @EventHandler
     private void onReceivePacket(PacketEvent.Receive event)
     {
@@ -64,32 +72,67 @@ public class AutoFish extends Module {
                 x = soundPacket.getX(); y = soundPacket.getY(); z = soundPacket.getZ();
         }
 
-        if (mc.player.fishHook.squaredDistanceTo(x, y, z) <= range.get() || mc.player.fishHook.getHookedEntity() != null)
-        {
-            tryCatch();
-            tryCast();
-        }
+        tryCast();
+        tryCatch();
     }
 
-    public void tryCatch()
+    private void tryCast()
     {
-        if (catchDelay != catchValue.get())
+        if (castDelayLeft > 0)
         {
-            catchDelay--;
-            return;   
+            castDelayLeft -= TickRate.INSTANCE.getTickRate() / 20.0;
+            return;
         }
-        catchDelay = catchValue.get();
+
+        useRod();
+    }
+
+
+    private void tryCatch()
+    {
+        if(mc.player.fishHook.squaredDistanceTo(x, y, z) <= range.get() || mc.player.fishHook.getHookedEntity() != null)
+            useRod();
+        
+        if (!hooked)
+        {
+            if (((FishingBobberEntityAccessor) mc.player.fishHook).meteor$hasCaughtFish()) 
+            {
+                catchDelayLeft = randomizeDelay(catchDelay.get(), catchDelayVariance.get());
+                hooked = true;
+            }
+            return;
+        }
+
+        if (catchDelayLeft > 0)
+        {
+            catchDelayLeft -= TickRate.INSTANCE.getTickRate() / 20.0;
+            return;
+        }
+
+        useRod();
+    }
+
+    private double randomizeDelay(int delay, int variance)
+    {
+        if (variance == 0) return delay;
+
+        // Sample the standard normal distribution via Box-Muller transform
+        double scale = Math.sqrt(-2 * Math.log(Utils.random(0.0001, 1.0)));
+        double angle = Math.TAU * Utils.random(0.0, 1.0);
+        double norm = scale * Math.cos(angle);
+
+        // Clamp to 3 standard deviations and re-scale to [-3.0, +3.0]
+        final double MAX_SD = 3.0;
+        norm = Math.clamp(norm, -MAX_SD, MAX_SD) / MAX_SD;
+
+        delay += Math.round((float)(norm * variance));
+        return Math.max(1, delay);
+    }
+
+    public void useRod()
+    {
         Utils.rightClick();
-    }
-
-    public void tryCast()
-    {
-        if (castDelay != castValue.get())
-        {
-            castDelay--;
-            return;   
-        }
-        castDelay = castValue.get();
-        Utils.rightClick();  
+        hooked = false;
+        castDelayLeft = randomizeDelay(castDelay.get(), castDelayVariance.get());
     }
 }
