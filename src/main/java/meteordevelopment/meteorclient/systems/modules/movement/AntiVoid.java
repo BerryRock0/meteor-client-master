1e6a8308c8341d0dd8777ee4deb02d5b808482d3
BerryRock0 <mdenials128@gmail.com>
2026-02-18 17:48:13 +0500

    Update AntiVoid.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/AntiVoid.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/AntiVoid.java
@@ -18,55 +18,17 @@ import meteordevelopment.orbit.EventHandler;
 public class AntiVoid extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
-        .name("mode")
-        .description("The method to prevent you from falling into the void.")
-        .defaultValue(Mode.Jump)
-        .onChanged(a -> onActivate())
-        .build()
-    );
-
     private boolean wasFlightEnabled, hasRun;
 
     public AntiVoid() {
         super(Categories.Movement, "anti-void", "Attempts to prevent you from falling into the void.");
     }
 
-    @Override
-    public void onActivate() {
-        if (mode.get() == Mode.Flight) wasFlightEnabled = Modules.get().isActive(Flight.class);
-    }
-
-    @Override
-    public void onDeactivate() {
-        if (!wasFlightEnabled && mode.get() == Mode.Flight && Utils.canUpdate()) {
-            Modules.get().get(Flight.class).disable();
-        }
-    }
-
     @EventHandler
     private void onPreTick(TickEvent.Pre event) {
         int minY = mc.world.getBottomY();
 
-        if (mc.player.getY() > minY || mc.player.getY() < minY - 15) {
-            if (hasRun && mode.get() == Mode.Flight) {
-                Modules.get().get(Flight.class).disable();
-                hasRun = false;
-            }
-            return;
-        }
+        if (mc.player.getY() > minY || mc.player.getY() < minY - 15) 
+           mc.player.jump(); 
 
-        switch (mode.get()) {
-            case Flight -> {
-                Modules.get().get(Flight.class).enable();
-                hasRun = true;
-            }
-            case Jump -> mc.player.jump();
-        }
-    }
-
-    public enum Mode {
-        Flight,
-        Jump
-    }
 }
