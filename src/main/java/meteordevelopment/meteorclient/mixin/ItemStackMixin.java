52d421c8b6b40b5c7b720402f2ddbd6f64968dc4
BerryRock0 <mdenials128@gmail.com>
2026-04-27 08:37:04 -0400

    Update ItemStackMixin.java

--- src/main/java/meteordevelopment/meteorclient/mixin/ItemStackMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/ItemStackMixin.java
@@ -16,6 +16,7 @@ import meteordevelopment.meteorclient.systems.modules.world.Damages;
 import meteordevelopment.meteorclient.systems.modules.world.Quantities;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
