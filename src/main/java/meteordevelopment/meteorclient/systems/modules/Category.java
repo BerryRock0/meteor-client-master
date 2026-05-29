fdb283508d1305a19cb77dc0ba450e45f70c44ed
BerryRock0 <mdenials128@gmail.com>
2026-05-29 18:40:26 +0000

    Update Category.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/Category.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/Category.java
@@ -5,20 +5,16 @@
 
 package meteordevelopment.meteorclient.systems.modules;
 
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-
 import java.util.function.Supplier;
 
 public class Category {
     public final String name;
-    public final Supplier<ItemStack> icon;
     private final int nameHash;
 
-    public Category(String name, Supplier<ItemStack> icon) {
+    public Category(String name)
+    {
         this.name = name;
         this.nameHash = name.hashCode();
-        this.icon = icon == null ? Items.AIR::getDefaultInstance : icon;
     }
 
     public Category(String name) {
