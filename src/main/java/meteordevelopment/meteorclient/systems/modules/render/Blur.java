acaff7465fda78d4ec3bea4744ac3ebf3ab03cfc
BerryRock0 <mdenials128@gmail.com>
2026-07-01 15:18:22 +0000

    Update Blur.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/Blur.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/Blur.java
@@ -5,6 +5,7 @@
 
 package meteordevelopment.meteorclient.systems.modules.render;
 
+import com.mojang.blaze3d.GpuFormat;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.mojang.blaze3d.buffers.Std140Builder;
 import com.mojang.blaze3d.buffers.Std140SizeCalculator;
@@ -12,7 +13,6 @@ import com.mojang.blaze3d.pipeline.RenderPipeline;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.textures.FilterMode;
 import com.mojang.blaze3d.textures.GpuTextureView;
-import com.mojang.blaze3d.GpuFormat;
 import it.unimi.dsi.fastutil.ints.IntFloatImmutablePair;
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.events.game.ResolutionChangedEvent;
@@ -199,7 +199,7 @@ public class Blur extends Module {
         }
 
         // Initial downsample
-        renderToFbo(fbos[0], mc.getMainRenderTarget().getColorTextureView(), MeteorRenderPipelines.BLUR_DOWN, ubos[0]);
+        renderToFbo(fbos[0], mc.gameRenderer.mainRenderTarget().getColorTextureView(), MeteorRenderPipelines.BLUR_DOWN, ubos[0]);
 
         // Downsample
         for (int i = 0; i < iterations; i++) {
@@ -213,7 +213,7 @@ public class Blur extends Module {
 
         // Render output
         MeshRenderer.begin()
-            .attachments(mc.getMainRenderTarget())
+            .attachments(mc.gameRenderer.mainRenderTarget())
             .pipeline(MeteorRenderPipelines.BLUR_PASSTHROUGH)
             .fullscreen()
             .sampler("u_Texture", fbos[0], RenderSystem.getSamplerCache().getClampToEdge(FilterMode.LINEAR)) // todo ???
@@ -232,7 +232,7 @@ public class Blur extends Module {
 
     private boolean shouldRender() {
         if (!isActive()) return false;
-        Screen screen = mc.screen;
+        Screen screen = mc.gui.screen();
 
         if (screen instanceof WidgetScreen) return meteor.get();
         if (screen instanceof AbstractContainerScreen) return inventories.get();
