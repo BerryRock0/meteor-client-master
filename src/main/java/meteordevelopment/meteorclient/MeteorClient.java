9521d53336f96724d8b03420aa702e51696cf37d
BerryRock0 <mdenials128@gmail.com>
2026-07-01 10:17:54 +0000

    Update MeteorClient.java

--- src/main/java/meteordevelopment/meteorclient/MeteorClient.java
+++ src/main/java/meteordevelopment/meteorclient/MeteorClient.java
@@ -19,7 +19,6 @@ import meteordevelopment.meteorclient.systems.config.Config;
 import meteordevelopment.meteorclient.systems.hud.screens.HudEditorScreen;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.misc.DiscordPresence;
 import meteordevelopment.meteorclient.utils.PostInit;
 import meteordevelopment.meteorclient.utils.PreInit;
 import meteordevelopment.meteorclient.utils.ReflectInit;
