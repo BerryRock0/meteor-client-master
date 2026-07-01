669a5f96ac7753ee7695911fa37951db1d8c76e7
BerryRock0 <mdenials128@gmail.com>
2026-07-01 21:16:19 +0000

    Update WallHack.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/WallHack.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/WallHack.java
@@ -29,7 +29,7 @@ public class WallHack extends Module {
         .sliderMax(255)
         .onChanged(_ -> {
             if (this.isActive()) {
-                mc.levelRenderer.invalidateCompiledGeometry(mc.level, mc.options, mc.gameRenderer.mainCamera(), mc.getBlockColors());
+                mc.levelExtractor.allChanged();
             }
         })
         .build()
@@ -40,7 +40,7 @@ public class WallHack extends Module {
         .description("What blocks should be targeted for Wall Hack.")
         .defaultValue()
         .onChanged(_ -> {
-            if (isActive()) mc.levelRenderer.invalidateCompiledGeometry(mc.level, mc.options, mc.gameRenderer.mainCamera(), mc.getBlockColors());
+            mc.levelExtractor.allChanged();
         })
         .build()
     );
@@ -58,12 +58,12 @@ public class WallHack extends Module {
 
     @Override
     public void onActivate() {
-        mc.levelRenderer.invalidateCompiledGeometry(mc.level, mc.options, mc.gameRenderer.mainCamera(), mc.getBlockColors());
+        mc.levelExtractor.allChanged();
     }
 
     @Override
     public void onDeactivate() {
-        mc.levelRenderer.invalidateCompiledGeometry(mc.level, mc.options, mc.gameRenderer.mainCamera(), mc.getBlockColors());
+        mc.levelExtractor.allChanged();
     }
 
     @Override
