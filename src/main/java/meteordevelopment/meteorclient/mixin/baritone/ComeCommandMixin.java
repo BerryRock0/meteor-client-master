d895020a37496af44a3caa17c03aa8ae5bb33ab0
BerryRock0 <mdenials128@gmail.com>
2026-07-10 10:07:38 +0000

    Update ComeCommandMixin.java

--- src/main/java/meteordevelopment/meteorclient/mixin/baritone/ComeCommandMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/baritone/ComeCommandMixin.java
@@ -18,12 +18,12 @@ import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 @Mixin(ComeCommand.class)
 public abstract class ComeCommandMixin {
-/*    @ModifyArgs(method = "execute", at = @At(value = "INVOKE", target = "Lbaritone/api/process/ICustomGoalProcess;setGoalAndPath(Lbaritone/api/pathing/goals/Goal;)V"), remap = false)
+    @ModifyArgs(method = "execute", at = @At(value = "INVOKE", target = "Lbaritone/api/process/ICustomGoalProcess;setGoalAndPath(Lbaritone/api/pathing/goals/Goal;)V"), remap = false)
     private void getComeCommandTarget(Args args) {
         Freecam freecam = Modules.get().get(Freecam.class);
         if (freecam.isActive()) {
             float tickDelta = mc.getDeltaTracker().getGameTimeDeltaPartialTick(true);
             args.set(0, new GoalBlock((int) freecam.getX(tickDelta), (int) freecam.getY(tickDelta), (int) freecam.getZ(tickDelta)));
         }
-    } */
+    }
 }
