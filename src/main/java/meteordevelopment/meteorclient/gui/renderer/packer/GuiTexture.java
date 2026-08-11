e42cfb48b3606ab730d0f5b068695c785eeab7d6
BerryRock0 <mdenials128@gmail.com>
2026-08-11 03:37:43 +0000

    Update GuiTexture.java

--- src/main/java/meteordevelopment/meteorclient/gui/renderer/packer/GuiTexture.java
+++ src/main/java/meteordevelopment/meteorclient/gui/renderer/packer/GuiTexture.java
@@ -11,25 +11,14 @@ import java.util.List;
 public class GuiTexture {
     private final List<TextureRegion> regions = new ArrayList<>(2);
 
-    void add(TextureRegion region) {
+    void add(TextureRegion region)
+    {   
         regions.add(region);
     }
 
-    public TextureRegion get(double width, double height) {
-        double targetDiagonal = Math.sqrt(width * width + height * height);
-
-        double closestDifference = Double.MAX_VALUE;
-        TextureRegion closestRegion = null;
-
-        for (TextureRegion region : regions) {
-            double difference = Math.abs(targetDiagonal - region.diagonal);
-
-            if (difference < closestDifference) {
-                closestDifference = difference;
-                closestRegion = region;
-            }
-        }
-
-        return closestRegion;
+    public TextureRegion get(double width, double height)
+    {
+        double targetDiagonal = Math.hypot(width, height);
+        return regions.stream().min((a, b) -> Double.compare(Math.abs(targetDiagonal - a.diagonal), Math.abs(targetDiagonal - b.diagonal))).orElse(null);
     }
 }
