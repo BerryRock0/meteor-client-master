6ac7e2ef695a9768c768be392e713971e904bc25
BerryRock0 <mdenials128@gmail.com>
2026-08-12 17:54:27 +0000

    Update KillAura.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/KillAura.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/KillAura.java
@@ -269,7 +269,7 @@ public class KillAura extends Module {
     public static int previousSlot;
 
     public KillAura() {
-        super(Categories.Combat, "kill-aura", "Attacks specified entities around you.");
+        super(Categories.Combat, "killaura", "Attacks specified entities around you.");
     }
 
     @Override
