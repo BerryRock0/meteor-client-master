284fd20163f1c648d991e40148e9819bc6b63068
BerryRock0 <mdenials128@gmail.com>
2026-08-12 19:53:31 +0000

    Create EntityCommand.java

---
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/EntityCommand.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.commands.commands;
+
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import meteordevelopment.meteorclient.commands.Command;
+
+public class EntityCommand extends Command
+{
+  public EntityCommand()
+  {
+    super("entity", "Entity manipulator.", "entity");
+  }
+
+  @Override
+  public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder)
+  {
+    builder.then(literal("help")).executes(_ -> {showHelp(this); return SINGLE_SUCCESS;}));
+    builder.then(literal("list")).executes(context -> {list(); return SINGLE_SUCCESS;}));
+    builder.then(literal("spawn")).executes(context -> {return SINGLE_SUCCESS;}));
+    builder.then(literal("remove")).executes(context -> {return SINGLE_SUCCESS;}));
+    builder.then(literal("teleport")).executes(context -> {return SINGLE_SUCCESS;}));
+    builder.then(literal("move")).executes(context -> {return SINGLE_SUCCESS;}));
+    builder.then(literal("edit")).executes(context -> {return SINGLE_SUCCESS;}));
+  }
+
+  public void list()
+  {
+    for(Entity entity: mc.level.entitiesForRendering())
+      info(entity.getUUID().toString());
+  }
+}
