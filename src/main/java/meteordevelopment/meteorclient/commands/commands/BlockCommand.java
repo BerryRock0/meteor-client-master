a5b47f65b4477be08f33329dd240c6806f797986
BerryRock0 <mdenials128@gmail.com>
2026-08-12 20:47:50 +0000

    Update BlockCommand.java

--- src/main/java/meteordevelopment/meteorclient/commands/commands/BlockCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/BlockCommand.java
@@ -20,11 +20,11 @@ public class BlockCommand extends Command
   @Override
   public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder)
   {
-    builder.then(literal("help")).executes(_ -> {showHelp(this); return SINGLE_SUCCESS;}));
-    builder.then(literal("search")).executes(context -> {return SINGLE_SUCCESS;}));
-    builder.then(literal("break")).executes(context -> {return SINGLE_SUCCESS;}));
-    builder.then(literal("interact")).executes(context -> {return SINGLE_SUCCESS;}));
-    builder.then(literal("place")).executes(context -> {return SINGLE_SUCCESS;}));
-    builder.then(literal("remove")).executes(context -> {return SINGLE_SUCCESS;}));
+    builder.then(literal("help")).executes(_ -> {showHelp(this); return SINGLE_SUCCESS;});
+    builder.then(literal("search")).executes(context -> {return SINGLE_SUCCESS;});
+    builder.then(literal("break")).executes(context -> {return SINGLE_SUCCESS;});
+    builder.then(literal("interact")).executes(context -> {return SINGLE_SUCCESS;});
+    builder.then(literal("place")).executes(context -> {return SINGLE_SUCCESS;});
+    builder.then(literal("remove")).executes(context -> {return SINGLE_SUCCESS;});
   }
 }
