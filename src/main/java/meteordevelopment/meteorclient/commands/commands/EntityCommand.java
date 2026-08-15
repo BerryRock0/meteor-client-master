ea5d2ecf4e9e36399ebdc4666e8ecf356f84bb94
BerryRock0 <mdenials128@gmail.com>
2026-08-15 03:52:21 +0000

    Update EntityCommand.java

--- src/main/java/meteordevelopment/meteorclient/commands/commands/EntityCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/EntityCommand.java
@@ -23,12 +23,12 @@ public class EntityCommand extends Command
   @Override
   public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder)
   {
-    builder.then(literal("list")).executes(context -> {list(); return SINGLE_SUCCESS;});
-    builder.then(literal("create")).executes(context -> {return SINGLE_SUCCESS;});
-    builder.then(literal("erase")).executes(context -> {return SINGLE_SUCCESS;});
-    builder.then(literal("teleport")).executes(context -> {return SINGLE_SUCCESS;});
-    builder.then(literal("tick")).executes(context -> {return SINGLE_SUCCESS;});  
-    builder.then(literal("move")).executes(context -> {return SINGLE_SUCCESS;});
+    builder.then(literal("list")).executes(_ -> {list(); return SINGLE_SUCCESS;});
+    builder.then(literal("create")).executes(_ -> {return SINGLE_SUCCESS;});
+    builder.then(literal("erase")).executes(_ -> {return SINGLE_SUCCESS;});
+    builder.then(literal("teleport")).executes(_ -> {return SINGLE_SUCCESS;});
+    builder.then(literal("tick")).executes(_ -> {return SINGLE_SUCCESS;});  
+    builder.then(literal("move")).executes(_ -> {return SINGLE_SUCCESS;});
   }
 
   public void list()
