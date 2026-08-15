510492fc6a550228bf6259f1bd1c7c459afd88c6
BerryRock0 <mdenials128@gmail.com>
2026-08-15 04:05:46 +0000

    Update EntityCommand.java

--- src/main/java/meteordevelopment/meteorclient/commands/commands/EntityCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/EntityCommand.java
@@ -24,11 +24,11 @@ public class EntityCommand extends Command
   public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder)
   {
     builder.then(literal("list")).executes(_ -> {list(); return SINGLE_SUCCESS;})
-    .then(literal("create")).executes(_ -> {return SINGLE_SUCCESS;})
-    .then(literal("erase")).executes(_ -> {return SINGLE_SUCCESS;})
-    .then(literal("teleport")).executes(_ -> {return SINGLE_SUCCESS;})
-    .then(literal("tick")).executes(_ -> {return SINGLE_SUCCESS;})
-    .then(literal("move")).executes(_ -> {return SINGLE_SUCCESS;});
+    .then(literal("create").executes(_ -> {return SINGLE_SUCCESS;}))
+    .then(literal("erase").executes(_ -> {return SINGLE_SUCCESS;}))
+    .then(literal("teleport").executes(_ -> {return SINGLE_SUCCESS;}))
+    .then(literal("tick").executes(_ -> {return SINGLE_SUCCESS;}))
+    .then(literal("move").executes(_ -> {return SINGLE_SUCCESS;}));
   }
 
   public void list()
