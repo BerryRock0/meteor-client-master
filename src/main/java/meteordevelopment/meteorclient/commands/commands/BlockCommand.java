77f04fd858c6ffc9be13f2e0285361a55075d558
BerryRock0 <mdenials128@gmail.com>
2026-08-15 03:53:05 +0000

    Update BlockCommand.java

--- src/main/java/meteordevelopment/meteorclient/commands/commands/BlockCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/BlockCommand.java
@@ -21,10 +21,10 @@ public class BlockCommand extends Command
   @Override
   public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder)
   {
-    builder.then(literal("search")).executes(context -> {return SINGLE_SUCCESS;});
-    builder.then(literal("break")).executes(context -> {return SINGLE_SUCCESS;});
-    builder.then(literal("interact")).executes(context -> {return SINGLE_SUCCESS;});
-    builder.then(literal("place")).executes(context -> {return SINGLE_SUCCESS;});
-    builder.then(literal("remove")).executes(context -> {return SINGLE_SUCCESS;});
+    builder.then(literal("search")).executes(_ -> {return SINGLE_SUCCESS;});
+    builder.then(literal("break")).executes(_ -> {return SINGLE_SUCCESS;});
+    builder.then(literal("interact")).executes(_ -> {return SINGLE_SUCCESS;});
+    builder.then(literal("place")).executes(_ -> {return SINGLE_SUCCESS;});
+    builder.then(literal("remove")).executes(_ -> {return SINGLE_SUCCESS;});
   }
 }
