a4ab74fb252779599f786d4422ea7b366373a10c
BerryRock0 <mdenials128@gmail.com>
2026-08-18 07:16:11 +0000

    Update ReverseStep.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/ReverseStep.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/ReverseStep.java
@@ -45,7 +45,7 @@ public class ReverseStep extends Module {
     );
 
     public ReverseStep() {
-        super(Categories.Movement, "reverse-step", "Allows you to fall down blocks at a greater speed.");
+        super(Categories.Movement, "reverseStep", "Allows you to fall down blocks at a greater speed.");
     }
 
     @EventHandler
