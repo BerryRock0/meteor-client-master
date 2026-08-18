bc077ef3588a4ee80a8878df5b015fa7fbfb9224
BerryRock0 <mdenials128@gmail.com>
2026-08-18 07:19:20 +0000

    Update AutoClicker.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/AutoClicker.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/AutoClicker.java
@@ -59,7 +59,7 @@ public class AutoClicker extends Module {
     private int rightClickTimer, leftClickTimer;
 
     public AutoClicker() {
-        super(Categories.Player, "auto-clicker", "Automatically clicks.");
+        super(Categories.Player, "autoClicker", "Automatically clicks.");
     }
 
     @Override
