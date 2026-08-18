11e8f91362ffdeafbbf7964d03592155806cedba
BerryRock0 <mdenials128@gmail.com>
2026-08-18 07:10:16 +0000

    Update PacketCanceller.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/PacketCanceller.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/PacketCanceller.java
@@ -36,7 +36,7 @@ public class PacketCanceller extends Module {
     );
 
     public PacketCanceller() {
-        super(Categories.Misc, "packet-canceller", "Allows you to cancel certain packets.");
+        super(Categories.Misc, "packetCanceller", "Allows you to cancel certain packets.");
         runInMainMenu = true;
     }
 
