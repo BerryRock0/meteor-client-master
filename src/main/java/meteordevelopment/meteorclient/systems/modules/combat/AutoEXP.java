9ebe179e8366791ef662dcfaf7eff1de9dd4b679
BerryRock0 <mdenials128@gmail.com>
2026-08-18 06:59:35 +0000

    Update AutoEXP.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoEXP.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoEXP.java
@@ -77,7 +77,7 @@ public class AutoEXP extends Module {
     private int repairingI;
 
     public AutoEXP() {
-        super(Categories.Combat, "autoexp", "Automatically repairs your armor and tools in pvp.");
+        super(Categories.Combat, "autoEXP", "Automatically repairs your armor and tools in pvp.");
     }
 
     @Override
