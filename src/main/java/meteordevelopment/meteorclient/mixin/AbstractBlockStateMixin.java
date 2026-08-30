5ca9c7ca5d412594cf52f06344dcf5f4e0498e42
BerryRock0 <mdenials128@gmail.com>
2026-08-30 09:06:58 +0000

    Update AbstractBlockStateMixin.java

--- src/main/java/meteordevelopment/meteorclient/mixin/AbstractBlockStateMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/AbstractBlockStateMixin.java
@@ -9,6 +9,10 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,8 +28,8 @@ public abstract class AbstractBlockStateMixin {
         if (Modules.get().get(NoRender.class).noTextureRotations()) cir.setReturnValue(Vec3.ZERO);
     }
 
-    @Inject(at = @At("HEAD"), method = "getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/shapes/VoxelShape;", cancellable = true)
-	private void onGetInteractionShape(BlockGetter view, BlockPos pos, CallbackInfoReturnable<VoxelShape> cir)
+    @Inject(at = @At("HEAD"), method = "getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", cancellable = true)
+	private void onGetInteractionShape(BlockGetter view, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir)
 	{
 	    if (Modules.get() == null) return;
         Collisions collision = Modules.get().get(Collisions.class);
