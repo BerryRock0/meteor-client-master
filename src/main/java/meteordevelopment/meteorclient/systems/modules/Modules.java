782791982a4fbf624e96eb1ad35c0025448ecd78
BerryRock0 <mdenials128@gmail.com>
2026-08-30 09:55:06 +0000

    Update Modules.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
@@ -448,7 +448,6 @@ public class Modules extends System<Modules> {
         add(new Multitask());
         add(new NameProtect());
         add(new NoInteract());
-        add(new NoMiningTrace());
         add(new NoRotate());
         add(new NoStatusEffects());
         add(new Portals());
